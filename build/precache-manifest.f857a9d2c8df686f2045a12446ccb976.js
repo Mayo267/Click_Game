fadae3a14e50b388ad0588b8135f68bcece95dc7
Andrew Mayo <amayo0925@gmail.com>
2019-04-06 10:22:25 -0400

    deploy npm

---
+++ build/precache-manifest.f857a9d2c8df686f2045a12446ccb976.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "942171fb6bfd5a35f105",
+    "url": "/Click_Game/static/js/runtime~main.d59bef99.js"
+  },
+  {
+    "revision": "3ad85dde88ca10b68e94",
+    "url": "/Click_Game/static/js/main.14171548.chunk.js"
+  },
+  {
+    "revision": "dd0ed99a380f8d225736",
+    "url": "/Click_Game/static/js/2.884c28c3.chunk.js"
+  },
+  {
+    "revision": "3ad85dde88ca10b68e94",
+    "url": "/Click_Game/static/css/main.140b4c69.chunk.css"
+  },
+  {
+    "revision": "516b41c68650b98832855e4842012b40",
+    "url": "/Click_Game/index.html"
+  }
+];
